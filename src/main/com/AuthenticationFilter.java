1e0570431a37be79c6d7c4dd13afc9b1e389eae3
atiqkhaled <atiqkhaled@gmail.com>
2017-03-07 20:50:16 +0600

    Update AuthenticationFilter.java

--- src/main/com/AuthenticationFilter.java
+++ src/main/com/AuthenticationFilter.java
@@ -2,4 +2,12 @@ package main.com;
 
 public class AuthenticationFilter {
     private String name;
+    
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
