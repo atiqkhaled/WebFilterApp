91bed3cb70f9a0d87febdd2bedc9212ebe1ac067
System Administrator <root@atiq-khaleds-MacBook-Pro.local>
2017-03-07 21:51:10 +0600

    update

--- src/main/com/AuthenticationFilter.java
+++ src/main/com/AuthenticationFilter.java
@@ -2,4 +2,19 @@ package main.com;
 
 public class AuthenticationFilter {
 
+   private String password;
+
+    private String name;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+    
+    
+
+
 }
